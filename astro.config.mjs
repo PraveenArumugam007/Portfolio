4fbf2948fe2338801a9f12243af4c1a32482a88f
Praveen_1317 <praveena@centizen.com>
2026-06-09 18:49:02 +0530

    deployment config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://praveena.github.io/Portfolio',
+  site: 'https://praveena.github.io',
+  base: '/Portfolio',
   markdown: {
     syntaxHighlight: 'shiki',
   },
